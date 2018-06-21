9d5e6f7a25f52fbaf7df655de6e5d222d2ef7a27
Erik Saule <esaule@uncc.edu>
2018-06-20 20:49:51 -0400

    adding an empty Game

---
+++ MinimalGame.java
@@ -0,0 +1,14 @@
+
+class MinimalGame extends NGCKGame {
+    
+    public MinimalGame (int assid, String login, String apiKey) {
+	super(assid, login, apiKey);
+    }
+    
+    public void initialize(){
+    }
+    
+    public void GameLoop(){
+    }
+
+}
